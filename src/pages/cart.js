76ea2501c7be5f281a6d494adee4ff119c4aa163
ohduran <alvaro.duranb@hotmail.com>
2020-05-22 17:19:33 +0200

    display contents of cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -8,10 +8,38 @@ const Cart = () => {
       checkout: { lineItems },
     },
   } = useContext(CartContext)
+  console.log(lineItems)
   return (
     <DefaultLayout>
       <h1>Cart</h1>
       <div>The cart length is {lineItems.length}</div>
+      <ul
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr",
+        }}
+      >
+        {lineItems.map(item => (
+          <li
+            style={{
+              listStyleType: "none",
+              textAlign: "center",
+            }}
+          >
+            <figure>
+              <img
+                src={item.variant.image.src}
+                style={{
+                  width: "100%",
+                }}
+              />
+            </figure>
+            <p>{item.title}</p>
+            <p>{item.variant.title}</p>
+            <p>{item.variant.price}€</p>
+          </li>
+        ))}
+      </ul>
     </DefaultLayout>
   )
 }
